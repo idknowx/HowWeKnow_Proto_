7edf884dfcab445c4edc0e8aff0590945517e662
idknow <joyoona4174@gmail.com>
2022-11-18 14:33:59 +0900

    빈 액티비티로 변경 및 로딩화면 추가
    basic activity가 생각보다 복잡해서 일단 다 밀고 빈 화면으로 만듦 (linear layout)
    logo_png png 이용해서 로딩화면 만듦
    앱 이름까지 있는 로고 넣어봤는데 왜인지 라운드로 로고가 들어가서 잘려서 일단은 그림만 있는 로고(logo_noname) 사용

---
+++ app/src/main/java/com/example/myapplication2/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(4000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
